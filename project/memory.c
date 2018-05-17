d34a542dc8e605a00b5cd72b40bd94e912b56230
Mattias Karlsson <mattias.karlsson91@hotmail.se>
2018-05-17 09:55:18 +0200

    started with memory

--- project/memory.c
+++ project/memory.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
-#include "memory.h"
+#include <unistd.h>
 
 typedef struct list_t list_t;
+typedef struct list_node list_node;
 
-struct list_t {
-  void* first;
+struct list_node {
+  list_node* prev;
+  list_node* next;
 };
 
 
-void* malloc(size_t* size)
+struct list_t {
+  list_node node;
+  size_t size;
+  void * block;
+};
+
+list_node
+void* malloc(size_t size)
 {
-  printf("DUUUUDE");
+  void* p;
+  p = sbrk(size);
+  if (p < 0) {
+    return NULL;
+  }
+
+  printf("ola");
+  return q;
+}
+
+void free(list_t* node) {
+
+}
+
+
+int main(){
+  printf("hallo han ar ju retarderad^^");
 }
