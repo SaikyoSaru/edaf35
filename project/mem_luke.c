862fc19eab6ee223af87e6cde5319558cf634e8e
Luke <elt14lpo@student.lu.se>
2018-05-18 09:22:58 +0200

    hej

--- project/mem_luke.c
+++ project/mem_luke.c
@@ -42,26 +42,26 @@ void extend_list(list_t* tail)
 
 list_t* create_slot()
 {
-  list_t* new_slot;
+  list_t* new_slot = new list_t();
   new_slot->size = 0;
   new_slot->next = NULL;
   new_slot->vacant = 1;
   return new_slot;
 }
 
-list_t* create_block(size_t size)
+void create_block(list_t* slot, size_t size)
 {
   list_t* new_block = sbrk(size + sizeof(list_t));
   new_block->size = size;
   new_block->vacant = 0;
-  return new_block;
+  new_block->next = slot->next;
 }
 
 
 void* malloc(size_t size)
 {
   list_t* p = first_free_slot();
-  list_t* p = create_block(size);
+  create_block(p, size);
 
   printf("ola");
   return q;
--- project/memory.c
+++ project/memory.c
@@ -2,56 +2,41 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+typedef struct list_t list_t;
 
 list_t* head = NULL;
-
-typedef struct list_t list_t;
 
 struct list_t {
   list_t next;
   size_t size;
-  bool vacant;
 };
 
 list_t* first_free_block()
 {
   if (head == NULL) {
-  //  init_list();
     return head;
   }
-
   list_t* p = head;
-  while (!p->vacant) {
-    if (p->next == NULL) {
-
-    }
+  while (p->next != NULL) {
     p = p->next;
   }
-  return p;
-
+  return p->next;
 }
 
 list_t* create_block(size_t size)
 {
-
   list_t* new_block = sbrk(size + sizeof(list_t));
-  list_t* p = first_free_block();
-
   new_block->size = size;
   new_block->next = NULL;
   new_block->vacant = 1;
-
-
-
+  return new_block;
 }
 
 
 void* malloc(size_t size)
 {
-
-  //list_t* p = first_free_block(size);
-
-  list_t* p = create_block(size);
+  list_t* next = first_free_block();
+  next = create_block(size);
 
   printf("ola");
   return q;
