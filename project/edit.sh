852bc1e1476707b7617d22c5be0291328d3ea188
elt14lpo <elt14lpo@student.lu.se>
2018-08-27 17:32:13 +0200

    remake auto-file, should be correct after flavius input

--- project/edit.sh
+++ project/edit.sh
@@ -1,2 +1,3 @@
-sed -i 's/version.c \\/&\n\tmalloc.c \\/' ./gawk-3.1.8/Makefile
-sed -i 's/version.\$/&\(OBJEXT\) malloc.\$/' ./gawk-3.1.8/Makefile
+sed -i 's/replace.c \\/&\n\tmy_malloc.c \\/' ./gawk-3.1.8/Makefile
+sed -i 's/replace.\$/&\(OBJEXT\) my_malloc.\$/' ./gawk-3.1.8/Makefile
+sed -i 's/replace.Po/&\ninclude .\/\$\(DEPDIR\)\/my_malloc.Po/' ./gawk-3.1.8/Makefile
