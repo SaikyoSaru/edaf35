c97483b4103efc5ac8711bf89cabfa6541f4d259
Luke Postema <elt14lpo@student.lu.se>
2018-08-29 14:42:27 +0200

    all done

--- project/buddy/my_malloc.c
+++ project/buddy/my_malloc.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-// #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include "my_malloc.h"
 #include <math.h>
+#include "my_malloc.h"
 
 #define META_SIZE sizeof(node_t)
-#define MAX_LEVEL 20 //17
-#define INIT_SIZE 1048576 //131072
+#define MAX_LEVEL 32
+#define INIT_SIZE (1L << MAX_LEVEL)
 
-void* heap;
+void* heap = NULL;
 
 node_t* freelist[MAX_LEVEL + 1];
 
@@ -28,16 +27,13 @@ int get_level(int size) {
 */
 void init()
 {
-  //fprintf(stderr, "init\n");
   for (int i = 0; i <= MAX_LEVEL; i++) {
     freelist[i] = NULL;
   }
-  // heap = sbrk(0);
-  // void* req = sbrk(INIT_SIZE);
   heap = sbrk(INIT_SIZE);
-  // if (!req) {
-  //   return;
-  // }
+  if (!heap) {
+    exit(0);
+  }
   node_t* p = (node_t*) heap;
   p->level = MAX_LEVEL;
   p->next = NULL;
@@ -51,15 +47,15 @@ void printer(void)
     p = freelist[i];
     while (p != NULL) {
       fprintf(stderr, "%d (%zu),", p->level, p);
-      //fprintf(stderr, "%d,", p->level);
+      fprintf(stderr, "%d,", p->level);
       p = p->next;
     }
     if (p == NULL) {
-      //fprintf(stderr, "-");
+      fprintf(stderr, "-");
     }
-    //fprintf(stderr, "  ");
+    fprintf(stderr, "  ");
   }
-  //fprintf(stderr, "\n");
+  fprintf(stderr, "\n");
 }
 
 /*
@@ -67,7 +63,6 @@ void printer(void)
 */
 void insert(node_t* node)
 {
-  //fprintf(stderr, "INSERT level:%d, add: %zu\n", node->level, node);
   node_t* p = freelist[node->level];
   node->next = NULL;
   if (p == NULL || p > node) {
@@ -87,7 +82,6 @@ void insert(node_t* node)
 */
 node_t* pop(int level)
 {
-  //fprintf(stderr, "%d\n", level);
   node_t* p = freelist[level];
   if (p->next == NULL) {
     freelist[level] = NULL;
@@ -103,20 +97,15 @@ node_t* pop(int level)
 */
 node_t* split(int high_level, int low_level)
 {
-  //fprintf(stderr, "SPLIT\n");
-  node_t* node = NULL;
-  node = pop(high_level);
+  node_t* node = pop(high_level);
   for (size_t i = high_level; i > low_level; i--) {
-    //fprintf(stderr, "level: %d\n", i);
-    //printer();
-    size_t size = 1 << i - 1; // pow(2, i+4-1);
-    node_t* new_node = (node_t*)((char*)node + size);
-    //fprintf(stderr, "node: %zu, new_node: %zu\n", node, new_node);
+    size_t new_size = (1L << i - 1);
+    node_t* new_node = (node_t*)((char*)node + new_size);
     new_node->level = i-1;
     new_node->next = NULL;
     node->level = i-1;
     node->next = NULL;
-    insert(new_node);
+    freelist[i-1] = new_node;
   }
   return node;
 }
@@ -126,25 +115,20 @@ node_t* split(int high_level, int low_level)
 */
 void merge(int level)
 {
-  //fprintf(stderr, "Merge level:%d\n", level);
-  node_t* p = freelist[level];
-  if (p->next == NULL || level >= MAX_LEVEL) {
-    //fprintf(stderr, "RETURN\n");
+  if (level >= MAX_LEVEL) {
     return;
   }
+  node_t* p = freelist[level];
   node_t* q = p;
-  //printf("McD %zu\n", p->next);
-  // printer();
   while (p->next != NULL) {
-    if ((((size_t) p->next) - ((size_t) p)) == 1 << level) {
+    if ((((size_t) p->next) - ((size_t) p)) == 1L << level && (((void*)p - heap) / (1L << level) % 2)) {
       if (((size_t) p) - ((size_t) q) == 0) {
         freelist[level] = p->next->next;
       } else {
         q->next = p->next->next;
       }
-      p->level+=1; //inc level
+      p->level+=1;
       p->next = NULL;
-      //fprintf(stderr, "p->level: \n", p->level);
       insert(p);
       merge(level + 1);
       return;
@@ -156,36 +140,20 @@ void merge(int level)
 
 void free(void* node)
 {
-  //printer();
-  if (node == NULL) {
+  if (node == NULL || heap == NULL) {
     return;
   }
   node_t* p = ((node_t*)node) - 1;
-  if (p == NULL) {
-    return;
-  }
-  //fprintf(stderr, "FREE %d\n", p->level);
-  int level = p->level;
-  //printer();
   insert(p);
-  //printer();
-  merge(level);
-  //printer();
+  merge(p->level);
 }
 
 void* malloc(size_t size)
 {
-  //fprintf(stderr, "MALLOC\n");
-  //fprintf(stderr, "size: %zu\n", size);
   if (size > INIT_SIZE - META_SIZE || size <= 0) {
-    //fprintf(stderr, "COMEON\n");
     return NULL;
   }
-  //printer();
-  // Allocate in power of 2 Our node_t is 32 bytes
   int level = get_level(size + META_SIZE);
-  //fprintf(stderr, "level for alloc shit:%d\n", level);
-  // int alloc_size = 2 << (n-1);
   node_t* p;
   node_t* node;
   int closest_free_level = -1;
@@ -194,44 +162,25 @@ void* malloc(size_t size)
     init();
   }
 
-  // int level = get_level(alloc_size);
-  if (freelist[level] != NULL) {
-    node = pop(level);
-  } else {
-    for (int i = level + 1; i <= MAX_LEVEL; i++) {
-      if(freelist[i] != NULL) {
-        closest_free_level = i;
-        break;
-      }
+  for (int i = level; i <= MAX_LEVEL; i++) {
+    if (freelist[i] != NULL) {
+      closest_free_level = i;
+      break;
     }
-    if (closest_free_level == -1) {
-      //Add on new memory
-      //fprintf(stderr, "MOOOOORE\n");
-      //  p = (node_t*) sbrk(0);
-      //  void* req = sbrk(INIT_SIZE);
-      p = sbrk(INIT_SIZE);
-      // if (!req) {
-      //   return NULL;
-      // }
-      p->level = MAX_LEVEL;
-      p->next = NULL;
-      freelist[MAX_LEVEL] = p;
-      closest_free_level = MAX_LEVEL;
-    }
-    node = split(closest_free_level, level);
   }
-  //printer();
-  return (node+1);
+
+  if (closest_free_level == -1) {
+    return NULL;
+  }
+
+  node = split(closest_free_level, level);
+
+  return (void*) (node + 1);
 }
 
 
 void* calloc(size_t nitems, size_t size)
 {
-  //fprintf(stderr, "CALLOC\n");
-  if (nitems == 0 || size == 0) {
-    //fprintf(stderr, "NO\n");
-    return NULL;
-  }
   void* p = malloc(nitems*size);
   if (p != NULL) {
     memset(p, 0, nitems*size);
@@ -241,13 +190,17 @@ void* calloc(size_t nitems, size_t size)
 
 void* realloc(void* ptr, size_t size)
 {
-  //fprintf(stderr, "REALLOC\n");
   if (!ptr) {
     return malloc(size);
   }
 
+  if (!size) {
+    free(ptr);
+    return NULL;
+  }
+
   node_t* q = ((node_t*)ptr) - 1;
-  int old_size = 1 << q->level;
+  size_t old_size = 1L << q->level;
 
   if (old_size - META_SIZE >= size) {
     return ptr;
@@ -255,7 +208,7 @@ void* realloc(void* ptr, size_t size)
 
   node_t* p = malloc(size);
   if (p != NULL) {
-    memcpy(p, q, old_size - META_SIZE);
+    memcpy(p, q+1, old_size - META_SIZE);
     free(q+1);
   }
 
--- project/edit.sh
+++ project/edit.sh
@@ -1,3 +1,4 @@
 sed -i 's/replace.c \\/&\n\tmy_malloc.c \\/' ./gawk-3.1.8/Makefile
 sed -i 's/replace.\$/&\(OBJEXT\) my_malloc.\$/' ./gawk-3.1.8/Makefile
 sed -i 's/replace.Po/&\ninclude .\/\$\(DEPDIR\)\/my_malloc.Po/' ./gawk-3.1.8/Makefile
+sed -i 's/O2/O1/g' ./gawk-3.1.8/Makefile
